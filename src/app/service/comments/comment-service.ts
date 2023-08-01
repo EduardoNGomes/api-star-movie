e0bdcf16161139be7a5bc8af3addd5b70426e73a
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-08-01 11:39:30 -0300

    Create Comment Service

---
+++ src/app/service/comments/comment-service.ts
@@ -0,0 +1,26 @@
+import {
+  CommentProps,
+  CommentRepository,
+} from '@/app/repository/comment-repository'
+
+export class CommentService {
+  constructor(private commentRepository: CommentRepository) {}
+
+  async createComment(data: CommentProps): Promise<CommentProps> {
+    const comment = await this.commentRepository.create(data)
+
+    return comment
+  }
+
+  async selectAll(movie_id: string) {
+    const comments = await this.commentRepository.findByMovieId(movie_id)
+
+    return comments
+  }
+
+  async delete(id: string) {
+    const msg = await this.commentRepository.delete(id)
+
+    return msg
+  }
+}
